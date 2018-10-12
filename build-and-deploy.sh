afc4ffb5e7cc3d622a5d3d6c5052815101416411
Radek Novak <radek.novak.cze@gmail.com>
2018-10-12 20:49:55 +0200

    Am I in West

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -6,7 +6,7 @@ COMMIT_MSG=${1-"autocommit $(date "+%Y-%m-%d %H:%M:%S")"}
 echo "Committing with message: $COMMIT_MSG"
 
 git add .
-git ci -m " $COMMIT_MSG"
+git ci -m "$COMMIT_MSG"
 git push origin master
 
 # builds the website into public/
---
+++ static/stuff/AmIinWest/precache-manifest.0ea7ea7e6b0bd0aa57364ac844d7bac4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c5d4b9b3fce54e42bf1",
+    "url": "/static/js/main.6c5d4b9b.chunk.js"
+  },
+  {
+    "revision": "6c02dbd23e56725bf2a8",
+    "url": "/static/js/1.6c02dbd2.chunk.js"
+  },
+  {
+    "revision": "6c5d4b9b3fce54e42bf1",
+    "url": "/static/css/main.e3ccd6a6.chunk.css"
+  },
+  {
+    "revision": "070fe6a09d0225d56f127be46e5adccb",
+    "url": "/index.html"
+  }
+];
