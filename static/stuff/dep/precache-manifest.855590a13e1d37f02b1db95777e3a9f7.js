834162641a73137969325cf94f1221fe806f6758
Radek Novak <radek.novak.cze@gmail.com>
2019-06-23 12:34:56 +0200

    autocommit 2019-06-23 12:34:56

---
+++ static/stuff/dep/precache-manifest.855590a13e1d37f02b1db95777e3a9f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6f5fb1913534ac1049870c666ebbba",
+    "url": "/stuff/dep/index.html"
+  },
+  {
+    "revision": "96426c07778da631cf91",
+    "url": "/stuff/dep/static/css/main.8a3e681d.chunk.css"
+  },
+  {
+    "revision": "61807c445a5472432fe8",
+    "url": "/stuff/dep/static/js/2.4b232d7a.chunk.js"
+  },
+  {
+    "revision": "96426c07778da631cf91",
+    "url": "/stuff/dep/static/js/main.44cc0f80.chunk.js"
+  },
+  {
+    "revision": "a6977f2f8c6508ec9b92",
+    "url": "/stuff/dep/static/js/runtime~main.f482fb60.js"
+  }
+]);
