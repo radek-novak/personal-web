470d2c47e5e55a2cd96479dc5d095f6f65a223f4
Radek Novak <radek.novak.cze@gmail.com>
2019-07-07 23:19:44 +0200

    autocommit 2019-07-07 23:19:44

---
+++ static/stuff/du-viz/precache-manifest.9b2c9a6ef1339f30d55c52611e799515.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94dda96650e5997d971767f822db9c6",
+    "url": "/stuff/du-viz/index.html"
+  },
+  {
+    "revision": "bf6280bb30cd0db3ebe3",
+    "url": "/stuff/du-viz/static/css/main.be14736f.chunk.css"
+  },
+  {
+    "revision": "c39b4516284374972143",
+    "url": "/stuff/du-viz/static/js/2.a015b095.chunk.js"
+  },
+  {
+    "revision": "bf6280bb30cd0db3ebe3",
+    "url": "/stuff/du-viz/static/js/main.68b45919.chunk.js"
+  },
+  {
+    "revision": "cf62c24ffb9663ddf824",
+    "url": "/stuff/du-viz/static/js/runtime~main.16b95462.js"
+  }
+]);
