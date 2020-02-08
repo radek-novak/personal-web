fb570590215e6cab41aeb533447be4bcb3027a4a
Radek Novak <radek.novak.cze@gmail.com>
2020-02-08 15:26:30 +0100

    change du-viz link

--- src/du-viz/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import HomePage from "./containers/HomePage";
-const App: React.FC = () => {
-  return <HomePage />;
-};
-
-export default App;
--- src/du-viz/src/components/PathSizeIndicator.js
+++
@@ -1,9 +0,0 @@
-import { pathToStr } from "../utils/helpers";
-import filesize from "filesize";
-
-const pathSize = ({ path, size }) =>
-  `${pathToStr(path)} - ${filesize(size || 0, {
-    base: 10
-  })}`;
-
-export default pathSize;
